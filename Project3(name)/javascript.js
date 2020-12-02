c9cb3b3cae224fd79671dc041354a56a2ee909b9
RustamArifin <72663154+tamtamcoding@users.noreply.github.com>
2020-12-02 18:38:54 +0800

    Add files via upload

---
+++ Project3(name)/javascript.js
@@ -0,0 +1,39 @@
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  
+});
+
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
+
+// window.onload = function(){
+//   var anchors = document.getElementsByTagName('a');
+//   for (var i=0; i<anchors.length; i++){
+//     anchors[i].setAttribute('target', '_blank');
+//   }}
